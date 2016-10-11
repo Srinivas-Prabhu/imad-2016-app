0203fa59abb0da628aacff9288188574f51b44b9
Srinivas-Prabhu <nvsrinivasprabhu@gmail.com>
2016-10-11 22:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // move the image
-var img = ocument.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function () {
    img.style.marginLeft = '100px' ;
 };
