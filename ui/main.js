3d95fa4308e0a7caf168c2098e3c305102cced3c
Srinivas-Prabhu <nvsrinivasprabhu@gmail.com>
2016-10-11 23:18:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick = function(){
 };
 
 //Submit name
-var nameInput = documnet.getElementById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.oneclick = function (){
