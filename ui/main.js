0955e85513795bc4cceaecb9402833de89973e2e
Srinivas-Prabhu <nvsrinivasprabhu@gmail.com>
2016-10-12 08:13:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,48 +1,48 @@
-//counter code
 var button = document.getElementById('counter');
-
 button.onclick = function(){
-   // Create a request object
-   var request = new XMLHttpRequest();
-   
-   
-   //capture the response and store it in a variable
-   request.onreadystatechange = function (){
-       if (request.readyState === XMLHttpRequest.DONE){
-           // take some action
-           if(request.status === 200){
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
-           }
-       }
-       //Not done yet
-   };
-   
-   // make the request
-    request.open('GET','http://srinivas-prabhu.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-//Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.oneclick = function (){
-  //Make a request to the server and send the name
+  var request = new XMLHttpRequest();
   
-  //Capture a list of names and render it as a list
-var names = ['name1','name2','name3','name4'];
-var list = '';
-for(var i=0;i< names.length; i++){
-    list += '<li>' + names[i] + '</li>';
-    }
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+  request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+          
+      }
+  }
+  };
+  
+  request.open('GET','http://srinivas-prabhu.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
+
 };
 
-
-
-
-
-
+var submit = document.getElementById('Submit_btn');
+submit.onclick = function () {
+     var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+           var names = request.responseText;
+           names = JSON.parse(names);
+           var list ='';
+            for(var i=0 ; i< names.length ; i++){
+                 list+= '<li>'+ names[i]+ '</li>' ;
+                }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+      }
+  }
+  };
+  var nameInput = document.getElementById('name');
+var name = nameInput.value;
+  request.open('GET','http://srinivas-prabhu.imad.hasura-app.io/submit-name?name='+ name,true);
+  request.send(null);
+  };
