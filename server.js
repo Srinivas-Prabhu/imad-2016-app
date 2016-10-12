34ce2991111c450ffbfffe7174ee64c2cfd2a1c1
Srinivas-Prabhu <nvsrinivasprabhu@gmail.com>
2016-10-12 08:16:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,34 +85,39 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function(req,res){
-   counter = counter + 1;
-   res.send(counter.toString());
+    counter = counter + 1;
+    res.send(counter.toString());
 });
-var names=[];
-app.get('/submit-name', function(req,res){
-  var name = req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName', function (req,res){
+app.get('/:articleName', function (req, res) {
+    // articleName == article-one
+    //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  res.send(createtemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
+
+var names = [];
+app.get('/submit-name/:name',function(req,res){
+   // get the name from the request
+   var name = req.params.name; 
+   
+       
+   names.push(name);
+   // JSON :Javascript Object Notation
+   res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
