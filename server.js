d34c25f1832314cda6cdeb6983676deaee5fac3c
Srinivas-Prabhu <nvsrinivasprabhu@gmail.com>
2016-11-19 23:22:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function createTemplate (data) {
               <div id="comment_form">
               </div>
               <div id="comments">
-                <center>Loading comments...</center>
+                <center>Loading comments.....</center>
               </div>
           </div>
           <script type="text/javascript" src="/ui/article.js"></script>
